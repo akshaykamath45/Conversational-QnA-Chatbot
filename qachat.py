f026c461c2889a8bee8230b6fb7798e55418b793
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-27 13:30:06 +0530

    feat : implementing chatbot

--- qachat.py
+++ qachat.py
@@ -21,3 +21,32 @@ def get_gemini_response(question):
 st.set_page_config(page_title="Conversational QnA")
 st.header("Conversational QnA Chatbot")
 st.subheader("Gemini LLM Application")
+
+# intializing session state for chat history if it doesn't exists
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
+
+
+input=st.text_input("Input:",key="input") # user input or question
+submit=st.button("Ask the question")
+
+if submit and input:
+    response=get_gemini_response(input)
+    st.session_state['chat_history'].append(("You",input))
+    st.subheader("The Response is ") 
+    
+    # process does not have to wait for the entire content to come from LLM
+    # whatever response it is sending , we will display it in the frontend screen
+    # stream=True -> we get access of all the streaming data
+    
+    complete_response=""
+    for chunk in response:
+        complete_response+=chunk.text
+    st.write(complete_response) # this will display the immideate response
+    st.session_state['chat_history'].append(("Bot",complete_response)) # this will append to chat history
+
+
+# displaying chat history 
+st.subheader("The Chat History is ")
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}", unsafe_allow_html=True)
